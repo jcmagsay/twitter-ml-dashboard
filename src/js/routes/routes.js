5e6c02a3164b6ced2bd9ad68a3670dc5b4e3fe7e
Jill Magsaysay <jcmagsay@gmail.com>
2020-03-01 23:55:21 -0700

    reorganize routes

---
+++ src/js/routes/routes.js
@@ -0,0 +1,17 @@
+import Home from 'ecosystems/home';
+import NotFound from 'ecosystems/not-found';
+
+const routes = [
+  {
+    'path': '/',
+    'name': 'Home',
+    'component': Home
+  },
+  {
+    'path': '/*',
+    'name': 'Not Found',
+    'component': NotFound
+  }
+];
+
+export default routes;
